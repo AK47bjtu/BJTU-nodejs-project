44f690a97c22d2535b14117c6139ce8bca2a410d
吴竹青 <1034304563@qq.com>
2015-01-17 09:22:29 +0800

    test2

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(function(err, req, res, next) {
     });
 });
 server.listen(process.env.PORT || 5000);
-console.log('Listening on port %d', server.address().port);
+// console.log('Listening on port %d', server.address().port);
 //
 //var server = app.listen((process.env.PORT || 5000), function() {
 //    console.log('Listening on port %d', server.address().port);
